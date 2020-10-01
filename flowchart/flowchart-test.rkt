c2dfe3b9fa1bb4aeaa9025e8be6e2f180f1adfa9
Alexandra Olemskaya <alexandra.olemskaya@gmail.com>
2020-10-01 03:07:59 +0300

    add flowchart interpreter

---
+++ flowchart/flowchart-test.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(require "flowchart.rkt")
+(require rackunit)
+
+(define get-value
+  '((read value)
+    (main (return value))
+  ))
+
+(check-equal? (flowchart-run get-value '(2)) '2)
+
+(define find-name
+  '((read name namelist valuelist)
+    (search (if (equal? name (car namelist)) found cont))
+    (cont (:= valuelist (cdr valuelist))
+          (:= namelist  (cdr namelist))
+          (goto search))
+    (found (return (car valuelist)))
+    ))
+
+; (print (flowchart-run find-name '(y (x y z) (1 2 3))))
+(check-equal? (flowchart-run find-name '(y (x y z) (1 2 3))) '2)
+
